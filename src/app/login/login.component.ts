9d6d84e77b24a7c92a4f88bad93d73e63ce44636
shahriar.zaman <shahriar.zaman@randomaccessinc.com>
2022-04-25 19:47:36 -0400

    added new files

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { style } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+
+  username:string;
+  password:string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  LoginUser()
+  {
+    if(this.username == "admin" && this.password == "admin")
+    {
+      alert("Login Successful");
+      
+      
+    }
+    else
+    {
+      alert("Login Failed");
+    
+    }
+  }
+  
+
+}
